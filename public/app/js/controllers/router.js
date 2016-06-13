32c086a8fda515e2ae3d4b5cbca290d46b9ab288
Suren <surenatoyan93@gmail.com>
2016-06-14 01:07:39 +0400

    FrontEnd: HTML, Backbone: Dinamicly change fb shar image - test:001

--- public/app/js/controllers/router.js
+++ public/app/js/controllers/router.js
@@ -26,6 +26,8 @@ function($, Backbone, _, Dispatcher) {
     open_tab: function(tab) {
       console.log('ok');
       $('meta[name=image]').attr('content', 'app/img/main.jpg');
+      $('meta[name=img_height]').attr('content', '316');
+      $('meta[name=img_width]').attr('content', '477');
       Dispatcher.trigger('open:' + tab);
     }
   });
