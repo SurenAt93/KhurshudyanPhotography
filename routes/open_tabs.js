a6d29f8ec94d4d88226b7fe9d99fd54ee5e13ff6
Suren <surenatoyan93@gmail.com>
2016-06-15 00:39:01 +0400

    Backend: Jade: Create view engine - Jade - 3

--- routes/open_tabs.js
+++ routes/open_tabs.js
@@ -2,5 +2,6 @@ var path = require('path');
 
 module.exports.get = function(req, res) {
   console.log(req.originalUrl);
-  res.render('index', {fb_img_url: 'app/img/main.jpg'});
+  res.render('index', {fb_img_url: 'app/img/main.jpg',
+                      originalUrl: req.originalUrl});
 }
